21dc78661b1a0f8c8d5d5a640da22a036de4fc4a
Ztez <thuonghongthai97@gmail.com>
2026-07-05 11:13:16 +0700

    v

---
+++ utils/supabase.ts
@@ -0,0 +1,10 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.VITE_SUPABASE_URL;
+const supabaseKey = process.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Missing VITE_SUPABASE_URL or VITE_SUPABASE_PUBLISHABLE_KEY');
+}
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
